80a14a45627fe42e6e3c496791a820c911115b63
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-01-12 01:57:37 -0500

    final touch

--- js/api.js
+++ js/api.js
@@ -1,4 +1,3 @@
-const loader = document.getElementById("loader");
 export const loadProjects = async () => {
   const url =
     "https://www.ramzikarkoub.com/wp-json/wp/v2/project?&acf_format=standard";
